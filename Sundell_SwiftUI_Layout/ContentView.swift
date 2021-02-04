97ce3fc83a1819fdb53be02674117aac2aa41cd3
Tom Flahive <TomPFlahive@gmail.com>
2021-02-03 18:46:21 -0800

    Initial Commit

---
+++ Sundell_SwiftUI_Layout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sundell_SwiftUI_Layout
+//
+//  Created by Tom Flahive on 2/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sundell_SwiftUI_Layout/Sundell_SwiftUI_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sundell_SwiftUI_LayoutApp.swift
+//  Sundell_SwiftUI_Layout
+//
+//  Created by Tom Flahive on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Sundell_SwiftUI_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
