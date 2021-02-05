9dd46754b5277e5d564a12d479957860c276d91f
Tom Flahive <TomPFlahive@gmail.com>
2021-02-05 08:16:19 -0800

    Commit after completing Part 1 of 3

--- Sundell_SwiftUI_Layout/ContentView.swift
+++ Sundell_SwiftUI_Layout/ContentView.swift
@@ -11,15 +11,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            VStack {
-                CalendarView()
-                Spacer()
-            }
-            .background(Color.yellow)
-            Text("Event Title")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .background(Color.purple)
+        VStack {
+            EventHeader()
             Spacer()
         }.padding()
     }
@@ -27,24 +20,59 @@ struct ContentView: View {
 
 struct CalendarView: View {
     var body: some View {
+        let eventIsVerified = true
+        
         Image(systemName: "calendar")
             .resizable()
             .frame(width: 50, height: 50)
             .padding()
-            .background(Color.red)
+            .background(Color.blue)
             .cornerRadius(10)
             .foregroundColor(.white)
+            .addVerifiedBadge(eventIsVerified)
     }
 }
 
-
-
-
+struct EventHeader: View {
+    var body: some View {
+        HStack (spacing: 15){
+            CalendarView()
+            VStack (alignment: .leading){
+                Text("Event Title")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    //.background(Color.purple)
+                Text("Location")
+                    //background((Color.orange))
+            }
+            Spacer()
+        }
+        .padding()
+        //.background(Color.blue)
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+extension View {
+    func addVerifiedBadge(_ isVerified: Bool) -> some View {
+        ZStack(alignment: .topTrailing) {
+            self
+
+            if isVerified {
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                    .offset(x: 3, y: -3)
+                    .foregroundColor(Color.red)
+            }
+        }
+    }
+}
+
+
 
 
